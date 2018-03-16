d2a199ef9dcbac82e43ac9420144bb3fed098714
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2018-03-16 19:20:57 +0100

    Fix quality flaw

--- expand_jobs.sh
+++ expand_jobs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 REPO=peachee-java-flat
 JOBS=jobs
+JOB_NAME_MAX_LENGTH=50
+
 rm -rf $REPO 2>/dev/null
 git clone https://github.com/dbolkensteyn/peachee-java-flat $REPO
-find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed \"s/%PROJECT-NAME%/\${project:\${#project}<50?0:-50}/; s/%PROJECT%/\$project/;\" job.yaml > $JOBS/job-\$project.yaml" _ {} \;
+find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed \"s/%PROJECT-NAME%/\${project:\${#project}<$JOB_NAME_MAX_LENGTH?0:-$JOB_NAME_MAX_LENGTH}/; s/%PROJECT%/\$project/;\" job.yaml > $JOBS/job-\$project.yaml" _ {} \;
