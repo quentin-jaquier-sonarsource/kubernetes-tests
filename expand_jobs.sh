178190061ea22fb9f8e47bc75e3233474cd91cdf
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2018-03-16 19:14:30 +0100

    Limit the length of generated job names

--- expand_jobs.sh
+++ expand_jobs.sh
@@ -3,4 +3,4 @@ REPO=peachee-java-flat
 JOBS=jobs
 rm -rf $REPO 2>/dev/null
 git clone https://github.com/dbolkensteyn/peachee-java-flat $REPO
-find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed s/%PROJECT%/\$project/ job.yaml > $JOBS/job-\$project.yaml" _ {} \;
+find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed \"s/%PROJECT-NAME%/\${project:\${#project}<50?0:-50}/; s/%PROJECT%/\$project/;\" job.yaml > $JOBS/job-\$project.yaml" _ {} \;
