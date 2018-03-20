8f0104ad921431cf96f63bb53bc0c73a3f8ed802
Dinesh Bolkensteyn <dinesh.bolkensteyn@sonarsource.com>
2018-03-20 19:21:55 +0100

    Expand all %PROJECT% within the same line

--- expand_jobs.sh
+++ expand_jobs.sh
@@ -7,4 +7,4 @@ rm -rf $JOBS
 mkdir $JOBS
 rm -rf $REPO 2>/dev/null
 git clone https://github.com/dbolkensteyn/peachee-java-flat $REPO
-find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed \"s/%PROJECT-NAME%/\${project:\${#project}<$JOB_NAME_MAX_LENGTH?0:-$JOB_NAME_MAX_LENGTH}/; s/%PROJECT%/\$project/;\" $1 > $JOBS/job-\$project.yaml" _ {} \;
+find $REPO/* -type d -exec sh -c "project=\${1#$REPO/}; sed \"s/%PROJECT-NAME%/\${project:\${#project}<$JOB_NAME_MAX_LENGTH?0:-$JOB_NAME_MAX_LENGTH}/g; s/%PROJECT%/\$project/g;\" $1 > $JOBS/job-\$project.yaml" _ {} \;
